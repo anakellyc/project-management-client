f120453a66e47cc2f315370fe7db3ff3cadad371
anakellyc <anakcampos@gmail.com>
2018-12-13 18:23:28 +0100

    edit project updated

--- src/components/projects/ProjectDetails.js
+++ src/components/projects/ProjectDetails.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
+import EditProject from "./EditProject";
 
 class ProjectDetails extends Component {
   constructor(props) {
@@ -25,11 +26,42 @@ class ProjectDetails extends Component {
       });
   };
 
+  renderEditForm = () => {
+    if (!this.state.title) {
+      this.getSingleProject();
+    } else {
+      return (
+        <EditProject
+          theProject={this.state}
+          getTheProject={this.getSingleProject}
+        />
+      );
+    }
+  };
+
+  // DELETE PROJECT:
+  deleteProject = id => {
+    const { params } = this.props.match;
+    axios
+      .delete(`http://localhost:5000/api/projects/${params.id}`)
+      .then(responseFromApi => {
+        console.log(responseFromApi);
+        this.props.history.push("/projects"); // !!!
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
+
   render() {
     return (
       <div>
         <h1>{this.state.title}</h1>
         <p>{this.state.description}</p>
+        <div>{this.renderEditForm()} </div>
+        <button onClick={() => this.deleteProject(this.state._id)}>
+          Delete project
+        </button>
         <Link to={"/projects"}>Back to projects</Link>
       </div>
     );
